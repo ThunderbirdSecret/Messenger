8d18dce03ec1d6da43144c53be30d3d8deda0907
Katarina Shalagina <j.milk@yandex.ru>
2022-12-09 15:19:12 +0300

    add all files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    content: [
+        "./src/**/**/**.{html,js,ts,tsx,hbs}",
+    ],
+    theme: {
+        container: {
+            center: true,
+        },
+        extend: {},
+    },
+    plugins: [],
+}
